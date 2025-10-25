75bd093c440f4e25a3e12cfdf65208df9e4c172c
Ying <42222462+leiqianwang@users.noreply.github.com>
2025-10-25 17:42:36 -0400

    Add files via upload

---
+++ StatTracker/StatTracker/src/test/java/com/example/StatTracker/StatTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StatTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
